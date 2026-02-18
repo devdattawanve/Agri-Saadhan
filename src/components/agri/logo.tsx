a76e505ed40fe76632a9afda51af5d334b792839
Sarthak Phopse <phopsesarthak@gmail.com>
2026-02-18 20:11:25 +0000

    https://image2url.com/r2/default/images/1771445380556-a24b5bdb-a223-4afa

--- src/components/agri/logo.tsx
+++ src/components/agri/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="/logo.png"
+      src="https://image2url.com/r2/default/images/1771445380556-a24b5bdb-a223-4afa-b35c-7223b22dff5c.png"
       alt="Agri Saadhan Logo"
       width={250}
       height={121}
