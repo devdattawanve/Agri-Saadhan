7f6aabdf268ba89c7c4d46eb4aeebc738d12659f
Sarthak Phopse <phopsesarthak@gmail.com>
2026-02-18 20:55:05 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/agri/logo.tsx
+++ src/components/agri/logo.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image';
-
 export function Logo() {
   return (
-    <Image
+    <img
       src="https://image2url.com/r2/default/images/1771447763339-21075360-936b-4d04-b2a4-eee74cf04e84.png"
       alt="Agri Saadhan Logo"
-      width={400}
-      height={194}
-      priority
+      width="400"
+      height="194"
+      style={{
+        color: 'transparent',
+        height: 'auto',
+        maxWidth: '100%',
+      }}
     />
   );
 }
