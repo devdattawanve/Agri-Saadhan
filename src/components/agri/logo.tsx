a5f72b528e8c9e75ca6a4d1cfc3deb05a596b431
Devdatta Wanve <devdattawanve12@gmail.com>
2026-02-18 21:24:48 +0000

    change this image with the given url.........  https://image2url.com/r2/

--- src/components/agri/logo.tsx
+++ src/components/agri/logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
     <Image
       src="https://image2url.com/r2/default/images/1771447763339-21075360-936b-4d04-b2a4-eee74cf04e84.png"
       alt="Agri Saadhan Logo"
-      width="400"
-      height="194"
+      width={400}
+      height={194}
       style={{
         color: 'transparent',
         height: 'auto',
