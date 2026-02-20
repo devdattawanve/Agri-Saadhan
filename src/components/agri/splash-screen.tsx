3ed0d6522c7ff3716b5ebb471dbaebca47da97b9
Arnav Wagh <arnavwagh36@gmail.com>
2026-02-20 13:09:55 +0000

    when app starts The logo will pop up at that time change the background

--- src/components/agri/splash-screen.tsx
+++ src/components/agri/splash-screen.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/components/agri/logo";
 
 export function SplashScreen() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-background">
+    <div className="flex h-screen w-screen items-center justify-center bg-white">
       <div className="animate-zoom-in-out">
         <Logo />
       </div>
